612936890ed044a1d19d3371201f514a6eef1e5a
pu2rile <3o920@naver.com>
2025-04-11 15:34:10 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/effective/festive/FestiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.effective.festive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestiveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/effective/festive/FestiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effective.festive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestiveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
